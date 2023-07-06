b33ae0e1af30f03ca4e53aac51e37c58cb384ae9
sunseo18 <sunseo1818@gmail.com>
2023-07-07 01:29:34 +0900

    [ADD] 페이지 스크롤 내리면서 화면 내 모든 서점 데이터 취합 & 휴무일 정보 취합

--- crawling_server.py
+++ crawling_server.py
@@ -1,11 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 import selenium.common.exceptions as Exceptions
 import time
-
 
 def sleep(second):
     time.sleep(second)
@@ -30,12 +29,33 @@ def switch_frame(frame_id):
     except Exceptions.NoSuchFrameException:
         print("ERROR: 프레임 전환 실패")
 
+def scroll_down(height):
+    try:
+        last_height=driver.execute_script("return document.body.scrollHeight")
+
+        driver.execute_script(f"window.scrollTo(0, {height});")
+        new_height=driver.execute_script("return document.body.scrollHeight")
+
+        if new_height == last_height:
+            return -1
+    except Exception:
+        pass
+
 switch_frame(frame_id="searchIframe")
 
-try: 
-    book_stores = driver.find_elements(By.CLASS_NAME, "place_bluelink")
+try:
+    book_stores = list()
 
+    for i in range(10):
+        book_stores = book_stores + driver.find_elements(By.CLASS_NAME, "place_bluelink")[i*10:]
+        last_book_store = book_stores[-1]
+        action = ActionChains(driver)
+        action.move_to_element(last_book_store).perform()
+        
+    print(len(book_stores))
+    
     for book_store in book_stores:
+
         book_store.click()
         sleep(5)
 
@@ -49,7 +69,9 @@ try:
         except Exceptions.NoSuchElementException:
             book_store_location = "null"
             
-        try:
+        book_store_closed_days=""
+        book_store_time=""
+        try: # 영업 시간 토글 있을 때
             book_store_time_div = driver.find_element(By.CLASS_NAME, "O8qbU.pSavy")
             book_store_time_toggle_span = book_store_time_div.find_element(By.CLASS_NAME, "_UCia")
             book_store_time_toggle_span.find_element(By.CLASS_NAME, "DNzQ2").click()
@@ -57,11 +79,13 @@ try:
             book_store_time_days = driver.find_elements(By.CLASS_NAME, "i8cJw")
             book_store_time_hours = driver.find_elements(By.CLASS_NAME, "H3ua4")
 
-            book_store_time=""
             for i in range(len(book_store_time_days)):
-                book_store_time += book_store_time_days[i].text + ": " + book_store_time_hours[i].text + " "
-            
+                if "정기휴무" in book_store_time_hours[i].text:
+                    book_store_closed_days += book_store_time_days[i].text + " "
+                else:
+                    book_store_time += book_store_time_days[i].text + "-> " + book_store_time_hours[i].text + " "
 
+        # 영업 시간 토글 없을 때
         except Exceptions.NoSuchElementException:
             try:
                 book_store_time = driver.find_element(By.CLASS_NAME, "U7pYf").text
@@ -73,7 +97,11 @@ try:
             book_store_phone = driver.find_element(By.CLASS_NAME, "xlx7Q").text
         except Exceptions.NoSuchElementException:
             book_store_phone = "null"
-        print(f"서점 이름: {book_store_title}, 주소: {book_store_location}, 운영시간: {book_store_time}, 번호: {book_store_phone}")
+
+        if not book_store_closed_days:
+            book_store_closed_days = "null"
+            
+        print(f"서점 이름: {book_store_title}, 주소: {book_store_location}, 운영시간: {book_store_time}, 휴무일: {book_store_closed_days}, 번호: {book_store_phone}")
 
         driver.switch_to.default_content()
         switch_frame(frame_id="searchIframe")
